71ebb3dcf139515a8cea03f8e213f8f00fd26187
siromanto <dmytrenko.serhii@gmail.com>
2019-03-15 17:30:53 +0200

    prepare load

--- etl/load.py
+++ etl/load.py
@@ -6,56 +6,60 @@ import pandas as pd
 from config import config, helpers
 
 
-class Load:
+def _execute_queries_for_upload(curr, report_path, storage_path, table_name):
+    curr.execute('PUT \'file://{}\' \'{}\''.format(report_path, storage_path))
+    curr.execute('COPY INTO {} FROM \'{}\' '
+                               'FILE_FORMAT=(SKIP_HEADER=1 FIELD_OPTIONALLY_ENCLOSED_BY=\'"\')'.format(table_name,
+                                                                                                       storage_path))
+    curr.execute('REMOVE \'{}\''.format(storage_path))
 
-    def __init__(self, con_config):
-        self.config = con_config
-        self.conn = self.create_connection()
 
-    def create_connection(self):
-        conn = helpers.establish_db_conn(
-            self.config.SNOWFLAKE_DB_USERNAME,
-            self.config.SNOWFLAKE_DB_PASSWORD,
-            self.config.SNOWFLAKE_DB_ACCOUNT,
-            self.config.SNOWFLAKE_DATABASE,
-            self.config.SNOWFLAKE_WAREHOUSE
-        )
-        return conn
+def load_data():
+    report_path = config.DATA_PATH
+    load_raw_data_from_csv(report_path)
 
-    def load_raw_data_from_csv(self, table_name):
 
-        for file in os.listdir('/Users/siromanto/edu/python/BingAds/data/'):
-            if file.endswith('.csv'):
-                print(f'Try to load file: {file} ===>')
+def load_weekly():
+    pass
 
-                file_path = f"/Users/siromanto/edu/python/BingAds/data/{file}"
 
-                curr = self.conn.cursor()
-                storage_path = '@%{}/{}'.format(table_name, file)
+def load_raw_data_from_csv(file_path):
+    file_name = file_path.rsplit('/', 1)[-1]
 
-                try:
-                    curr.execute('BEGIN')
-                    # self._cleanup_data(curr, table_name)
-                    self._execute_queries_for_upload(file_path, storage_path, table_name)
-                    curr.execute('COMMIT')
+    client_config = helpers.get_client_config(r'/Users/siromanto/ralabs/0.projects/conDati/BingAds/config/BingConsole.json')
+    db_config = helpers.get_client_config('../config/Siromanto_account.json')
 
-                    print(f'FINISH FILE LOADING...')
-                except Exception as e:
-                    print(e)
+    # client_config = helpers.get_client_config(r'/opt/workbench/users/afuser/airflow/dags/credentials/BingConsole/Toweltech.json')
+    # db_config = helpers.get_client_config('credentials/SnowflakeKeys/CONDATI ----> .json')
 
-        self.conn.cursor().close()
-        self.conn.close()
+    conn = helpers.establish_db_conn(
+        db_config['user'],
+        db_config['pwd'],
+        db_config['account'],
+        client_config['raw_db'],  # Change this in prod env
+        client_config['warehouse']  # Change this in prod env
+    )
+
+    curr = conn.cursor()
+    table_name = client_config['raw_table']
+
+    storage_path = '@%{}/{}'.format(table_name, file_name)
+    try:
+        curr.execute('BEGIN')
+        # _cleanup_data(curr, table_name)
+        _execute_queries_for_upload(curr, file_path, storage_path, table_name)
+        curr.execute('COMMIT')
+
+        print(f'FINISH FILE LOADING...')
+
+    except Exception as e:
+        print(e)
+    finally:
+        conn.cursor().close()
+        conn.close()
         print(f"Data imported successfully")
-
-    def _execute_queries_for_upload(self, report_path, storage_path, table_name):
-        self.conn.cursor().execute('PUT \'file://{}\' \'{}\''.format(report_path, storage_path))
-        self.conn.cursor().execute('COPY INTO {} FROM \'{}\' '
-                                   'FILE_FORMAT=(SKIP_HEADER=1 FIELD_OPTIONALLY_ENCLOSED_BY=\'"\')'.format(table_name, storage_path))
-        self.conn.cursor().execute('REMOVE \'{}\''.format(storage_path))
-
-    def load(self, table_name=None):
-        self.load_raw_data_from_csv(table_name)
+    # os.remove(file_path)
 
 
 if __name__ == '__main__':
-    Load(config).load(table_name='BING_WEBMASTER_QUERY_STATS')
+    load_data()
