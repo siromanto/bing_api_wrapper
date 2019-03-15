9cc86dc039761e6ad9b2734285239ed08e040b80
siromanto <dmytrenko.serhii@gmail.com>
2019-03-15 13:13:51 +0200

    prepare extract

--- etl/extract.py
+++ etl/extract.py
@@ -5,21 +5,26 @@ from config import config, helpers
 
 
 def extract_weekly():
-    pass
+    extract_data()
 
 
 def extract_data():
-    api = BingWebmasterApi()
-    pages_raw_data = api.GetPageStats(siteUrl=config.BING_SITE_URL)
+    credentials = helpers.get_client_config(conf_path=r'/Users/siromanto/ralabs/0.projects/conDati/BingAds/config/BingSearchConsole.json')
+    # credentials = helpers.get_client_config(conf_path=r'/opt/workbench/users/afuser/airflow/dags/credentials/AmazonAdsKeys/Toweltech.json')
+
+    api = BingWebmasterApi(api_key=credentials["api_key"])
+    pages_raw_data = api.GetPageStats(siteUrl=credentials["bing_site_url"])
 
     global_info_pages = {x.get('Query'): x for x in pages_raw_data.get('d')}
 
-    with open(f'../data/{"Get_Page_Query_Stats".lower()}.csv', mode='w', encoding='utf8') as raw_csv:
+    with open(config.DATA_PATH, mode='w', encoding='utf8') as raw_csv:
         writer = helpers.prepare_header_for_clear_csv(raw_csv, helpers.CSV_COLUMNS)
+        print(f'find pages --- {len(global_info_pages)}')
 
         for target_page, page_data in global_info_pages.items():
-            page_detailed_info = api.GetPageQueryStats(siteUrl=config.BING_SITE_URL, page=target_page).get('d')
             row = {}
+            page_detailed_info = api.GetPageQueryStats(siteUrl=credentials["bing_site_url"], page=target_page).get('d')
+            print(f'Start working with url --- {target_page}...')
 
             row.update({
                 'SOURCE': page_data.get('__type').split('#')[1],
@@ -36,23 +41,22 @@ def extract_data():
                 'QUERY': None
             })
 
+            print(f'Url has atached queries ---> {len(page_detailed_info)}')
             if page_detailed_info:
-                # print(f'WE HAVE ATACHED DATA ---> {len(page_detailed_info)}')
                 for detailed_info in page_detailed_info:
                     row.update({
                         'QUERY_AVG_CLICK_POSITION': detailed_info.get('AvgClickPosition'),
                         'QUERY_AVG_IMPRESSION_POSITION': detailed_info.get('AvgImpressionPosition'),
-                        'DATE': detailed_info.get('Date'),
+                        'DATE': helpers.parse_date(detailed_info.get('Date')),
                         'QUERY_CLICKS': detailed_info.get('Clicks'),
                         'QUERY_IMPRESSIONS': detailed_info.get('Impressions'),
                         'QUERY': detailed_info.get('Query')
                     })
-
                     writer.writerow(row)
             else:
-                row.update(DATE=page_data.get('Date'))
+                row.update(DATE=helpers.parse_date(page_data.get('Date')))
                 writer.writerow(row)
 
 
 if __name__ == '__main__':
-    extract_data()
+    extract_weekly()
