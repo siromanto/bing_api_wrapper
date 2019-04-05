82ee9ab7e7f519e4954420b027f08ec0e3b66967
siromanto <dmytrenko.serhii@gmail.com>
2019-04-05 15:04:48 +0300

    fix config

--- configs/config.py
+++ configs/config.py
@@ -1,24 +1,24 @@
 # Development path TODO: Switch to prod path in prod env
 # DATA_PATH = '../data/bing_webmaster_data.csv'
-# CLIENT_CONFIG_PATH = r'/Users/siromanto/ralabs/0.projects/conDati/LinkedinAds/credentials/LinkedinAdsKeys/BingConsole_dev.json'
-# DB_CONFIG_PATH = r'/Users/siromanto/ralabs/0.projects/conDati/LinkedinAds/credentials/SnowflakeKeys/Siromanto_account.json'
+# CLIENT_CONFIG_PATH = r'/Users/siromanto/ralabs/0.projects/conDati/BingSearchConsole/credentials/BingSearchConsoleKeys/BingConsole_dev.json'
+# DB_CONFIG_PATH = r'/Users/siromanto/ralabs/0.projects/conDati/BingSearchConsole/credentials/SnowflakeKeys/Siromanto_account.json'
 
 
 # Production dev env
 DATA_PATH = '../data/bing_webmaster_data.csv'
-CLIENT_CONFIG_PATH = r'/Users/siromanto/ralabs/0.projects/conDati/LinkedinAds/credentials/BingSearchConsole/BingConsole_prod_test.json'
-DB_CONFIG_PATH = r'/Users/siromanto/ralabs/0.projects/conDati/LinkedinAds/credentials/SnowflakeKeys/SnowflakeComputing.json'
+CLIENT_CONFIG_PATH = r'/Users/siromanto/ralabs/0.projects/conDati/BingSearchConsole/credentials/BingSearchConsoleKeys/BingConsole_prod_test.json'
+DB_CONFIG_PATH = r'/Users/siromanto/ralabs/0.projects/conDati/BingSearchConsole/credentials/SnowflakeKeys/SnowflakeComputing.json'
 
 
 # Production airflow test path
 # DATA_PATH = 'data/bing_webmaster_data.csv'
-# CLIENT_CONFIG_PATH = r'/opt/workbench/users/afuser/airflow/dags/credentials/BingSearchConsole/BingConsole_prod_test.json'
+# CLIENT_CONFIG_PATH = r'/opt/workbench/users/afuser/airflow/dags/credentials/BingSearchConsoleKeys/BingConsole_prod_test.json'
 # DB_CONFIG_PATH = 'credentials/SnowflakeKeys/SnowflakeComputing.json'
 
 
 # Production path
 # DATA_PATH = 'data/bing_webmaster_data.csv'
-# CLIENT_CONFIG_PATH = r'/opt/workbench/users/afuser/airflow/dags/credentials/BingSearchConsole/BingConsole.json'
+# CLIENT_CONFIG_PATH = r'/opt/workbench/users/afuser/airflow/dags/credentials/BingSearchConsoleKeys/BingConsole.json'
 # DB_CONFIG_PATH = 'credentials/SnowflakeKeys/SnowflakeComputing.json'
 
 
